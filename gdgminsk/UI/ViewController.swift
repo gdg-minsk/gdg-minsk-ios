9b0df1fb94a4b01f765aa9ebd99433b18968b9c9
Anton Paliakov <toxa95401@gmail.com>
2020-05-01 15:55:48 +0300

    Init iOS project.

---
+++ gdgminsk/UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gdgminsk
+//
+//  Created by Anton Polyakov on 5/1/20.
+//  Copyright © 2020 gdgminsk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
